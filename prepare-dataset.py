74c60d74075ca1dab2868d556c1f5cbe12b56ffe
Cristian Padurariu <cristian9004@Cristians-MacBook-Pro.local>
2019-12-17 15:36:46 +0200

    added model saving and loading

--- prepare-dataset.py
+++ prepare-dataset.py
@@ -1,30 +1,35 @@
 import io
+from random import uniform
 
 with io.open("new_words.txt", "r", encoding="utf8") as f:
     words = f.readlines()
 
 current_pos = 0
 i=3
-with io.open("dataset.txt", "w", encoding="utf8") as f:
-    while current_pos < len(words) - 1:
-        current_words = words[current_pos:current_pos+i]
-        current_words = list(map(lambda x: x.strip(), current_words))
-        concatenated_words = ''.join(current_words)
-        current_pos += i
-        i+=1
-        if i == 8:
-            i = 3
+train = io.open("train_set.txt", "w", encoding="utf8")
+test = io.open("test_set.txt", "w", encoding="utf8")
+while current_pos < len(words) - 1:
+    current_words = words[current_pos:current_pos+i]
+    current_words = list(map(lambda x: x.strip(), current_words))
+    concatenated_words = ''.join(current_words)
+    current_pos += i
+    i+=1
+    if i == 8:
+        i = 3
 
-        target = ""
-        for word in current_words:
-            if len(word)>1:
-                target += 'i'
-                target += ''.join(['m'] * (len(word) - 2))
-                target += 's'
-            else:
-                target += 'c'
-        
-        f.write('|'.join(current_words) + " " +concatenated_words + " " + target + '\n')
-
+    target = ""
+    for word in current_words:
+        if len(word)>1:
+            target += 'i'
+            target += ''.join(['m'] * (len(word) - 2))
+            target += 's'
+        else:
+            target += 'c'
+    if uniform(0, 1) < 0.8: 
+        train.write('|'.join(current_words) + " " +concatenated_words + " " + target + '\n')
+    else:
+        test.write('|'.join(current_words) + " " +concatenated_words + " " + target + '\n')
+train.close()
+test.close()
     
     
--- train.py
+++ train.py
@@ -1,191 +1,237 @@
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
+from keras.models import model_from_json
 import numpy as np
 import io
+import pickle
 
 batch_size = 100  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 
-input_texts = []
-target_texts = []
-target_words = []
-input_characters = set()
-target_characters = set()
+class WordSplitter:
 
-with io.open("dataset.txt", "r", encoding="utf-8") as f:
+    def __init__(self, input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test):
+        self.input_texts_train = input_texts_train
+        self.target_texts_train = target_texts_train
+        self.target_words_train = target_words_train
+        self.input_texts_test = input_texts_test
+        self.target_texts_test = target_texts_test
+        self.target_words_test = target_words_test
+        self.encoder_input_data_test = None
+        self.decoder_input_data_test = None
+        self.decoder_target_data_test = None
+    
+    def prepare_data(self):
+        input_characters = set()
+        target_characters = set()
+        for input_text, target_text in list(zip(self.input_texts_train, self.target_texts_train)):
+            for char in input_text:
+                if char not in input_characters:
+                    input_characters.add(char)
+            for char in target_text:
+                if char not in target_characters:
+                    target_characters.add(char)
+
+        input_characters = sorted(list(input_characters))
+        target_characters = sorted(list(target_characters))
+        self.num_encoder_tokens = len(input_characters)+1
+        self.num_decoder_tokens = len(target_characters)+1
+        self.max_encoder_seq_length = max([len(txt) for txt in self.input_texts_train])+1
+        self.max_decoder_seq_length = max([len(txt) for txt in self.target_texts_train])+1
+
+        self.input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
+        self.target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+        self.input_token_index[' '] = len(self.input_token_index)
+        self.target_token_index[' '] = len(self.target_token_index)
+
+        self.reverse_target_char_index = dict(
+            (i, char) for char, i in self.target_token_index.items())
+        pickle.dump(self.reverse_target_char_index, open("reverse_target_char_index.p", "wb"))
+
+        self.encoder_input_data_train, self.decoder_input_data_train, self.decoder_target_data_train = self.get_network_ready_data(self.input_texts_train, self.target_texts_train)
+        self.encoder_input_data_test, self.decoder_input_data_test, self.decoder_target_data_test = self.get_network_ready_data(self.input_texts_test, self.target_texts_test)
+
+    def get_network_ready_data(self, input_texts, target_texts):
+        encoder_input_data = np.zeros(
+            (len(input_texts), self.max_encoder_seq_length, self.num_encoder_tokens),
+            dtype='float32')
+        decoder_input_data = np.zeros(
+            (len(input_texts), self.max_decoder_seq_length, self.num_decoder_tokens),
+            dtype='float32')
+        decoder_target_data = np.zeros(
+            (len(input_texts), self.max_decoder_seq_length, self.num_decoder_tokens),
+            dtype='float32')
+        for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+            for t, char in enumerate(input_text):
+                encoder_input_data[i, t, self.input_token_index[char]] = 1.
+            encoder_input_data[i, t + 1:, self.input_token_index[' ']] = 1.
+            for t, char in enumerate(target_text):
+                # decoder_target_data is ahead of decoder_input_data by one timestep
+                decoder_input_data[i, t, self.target_token_index[char]] = 1.
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    decoder_target_data[i, t - 1, self.target_token_index[char]] = 1.
+            decoder_input_data[i, t + 1:, self.target_token_index[' ']] = 1.
+            decoder_target_data[i, t:, self.target_token_index[' ']] = 1.
+        
+        return encoder_input_data, decoder_input_data, decoder_target_data
+
+    def __build_network(self):
+        # Define an input sequence and process it.
+        encoder_inputs = Input(shape=(None, self.num_encoder_tokens))
+        encoder = LSTM(latent_dim, return_state=True)
+        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        encoder_states = [state_h, state_c]
+
+        decoder_inputs = Input(shape=(None, self.num_decoder_tokens))
+        decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+        decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                            initial_state=encoder_states)
+        decoder_dense = Dense(self.num_decoder_tokens, activation='softmax')
+        decoder_outputs = decoder_dense(decoder_outputs)
+
+        self.model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+        self.encoder_model = Model(encoder_inputs, encoder_states)
+
+        decoder_state_input_h = Input(shape=(latent_dim,))
+        decoder_state_input_c = Input(shape=(latent_dim,))
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+        decoder_outputs, state_h, state_c = decoder_lstm(
+            decoder_inputs, initial_state=decoder_states_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        self.decoder_model = Model(
+            [decoder_inputs] + decoder_states_inputs,
+            [decoder_outputs] + decoder_states)
+
+    
+    def save_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file):
+        model_json = self.encoder_model.to_json()
+        with open(encoder_model_file, "w") as json_file:
+            json_file.write(model_json)
+        model_json = self.decoder_model.to_json()
+        with open(decoder_model_file, "w") as json_file:
+            json_file.write(model_json)
+        self.encoder_model.save(encoder_weights_file)
+        self.decoder_model.save(decoder_weights_file)
+    
+    def train(self, ):
+
+        self.__build_network()
+        
+        # Run training
+        self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+
+        self.model.fit([self.encoder_input_data_train, self.decoder_input_data_train], self.decoder_target_data_train,
+                batch_size=batch_size,
+                epochs=epochs,
+                validation_data=([self.encoder_input_data_test, self.decoder_input_data_test], self.decoder_target_data_test))
+
+
+    def load_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file, reverse_target_char_index_file):
+
+        self.reverse_target_char_index = pickle.load(open(reverse_target_char_index_file, "rb"))
+        loaded_encoder_json = open(encoder_model_file, "r").read()
+        loaded_decoder_json = open(decoder_model_file, "r").read()
+        self.encoder_model = model_from_json(loaded_encoder_json)
+        self.decoder_model = model_from_json(loaded_decoder_json)
+        self.encoder_model.load_weights(encoder_weights_file)
+        self.decoder_model.load_weights(decoder_weights_file)
+
+    def decode_sequence(self, input_seq, max_length):
+        states_value = self.encoder_model.predict(input_seq)
+        target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+        target_seq[0, 0, self.target_token_index['\t']] = 1.
+
+        stop_condition = False
+        decoded_sentence = ''
+        while not stop_condition:
+            output_tokens, h, c = self.decoder_model.predict(
+                [target_seq] + states_value)
+
+            # Sample a token
+            sampled_token_index = np.argmax(output_tokens[0, -1, :])
+            sampled_char = self.reverse_target_char_index[sampled_token_index]
+            decoded_sentence += sampled_char
+
+            # Exit condition: either hit max length
+            # or find stop character.
+            if (sampled_char == '\n' or
+            len(decoded_sentence) > max_length-1):
+                stop_condition = True
+
+            # Update the target sequence (of length 1).
+            target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+            target_seq[0, 0, sampled_token_index] = 1.
+
+            # Update states
+            states_value = [h, c]
+
+        return decoded_sentence
+
+
+    def decode_words(self, text, predicted):
+        decoded_words = ""
+        for i, p in enumerate(predicted):
+            if p == 'i':
+                decoded_words+=' '+text[i]
+            elif p == 's':
+                decoded_words+=text[i]+' '
+            else:
+                decoded_words+=text[i]
+        return decoded_words
+
+    def predict_on_test(self):
+        for seq_index in range(len(self.input_texts_train)):
+            # Take one sequence (part of the training set)
+            # for trying out decoding.
+            input_seq = self.encoder_input_data_test[seq_index: seq_index + 1]
+            decoded_string = self.decode_sequence(input_seq, len(self.input_texts_test[seq_index]))
+            print('-')
+            print('Input words:' , ' '.join(self.target_words_test[seq_index].split("|")))
+            print('Input string:', self.input_texts_test[seq_index])
+            print('Decoded string:', decoded_string)
+            print('Decoded words:', self.decode_words(self.input_texts_test[seq_index], decoded_string))
+
+
+        
+input_texts_train = []
+target_texts_train = []
+target_words_train = []
+
+with io.open("train_set.txt", "r", encoding="utf-8") as f:
     samples = f.readlines()
     for sample in samples:
         target_word, input_text, target_text = sample.split(" ")
         target_text = '\t' + target_text + '\n'
-        input_texts.append(input_text)
-        target_texts.append(target_text)
-        target_words.append(target_word)
-        for char in input_text:
-            if char not in input_characters:
-                input_characters.add(char)
-        for char in target_text:
-            if char not in target_characters:
-                target_characters.add(char)
-
-input_characters = sorted(list(input_characters))
-target_characters = sorted(list(target_characters))
-num_encoder_tokens = len(input_characters)+1
-num_decoder_tokens = len(target_characters)+1
-max_encoder_seq_length = max([len(txt) for txt in input_texts])+1
-max_decoder_seq_length = max([len(txt) for txt in target_texts])+1
-
-input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
-target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
-input_token_index[' '] = len(input_token_index)
-target_token_index[' '] = len(target_token_index)
-
-encoder_input_data = np.zeros(
-    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
-    dtype='float32')
-decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
-decoder_target_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
+        input_texts_train.append(input_text)
+        target_texts_train.append(target_text)
+        target_words_train.append(target_word)
 
 
-for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-    for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.
-    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
-    for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
-    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
-    decoder_target_data[i, t:, target_token_index[' ']] = 1.
-# Define an input sequence and process it.
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-# We discard `encoder_outputs` and only keep the states.
-encoder_states = [state_h, state_c]
+input_texts_test = []
+target_texts_test = []
+target_words_test = []
 
-# Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-# We set up our decoder to return full output sequences,
-# and to return internal states as well. We don't use the
-# return states in the training model, but we will use them in inference.
-decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
-                                     initial_state=encoder_states)
-decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
-
-# Define the model that will turn
-# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
-# Run training
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.2)
-# Save model
-model.save('s2s.h5')
-
-# Next: inference mode (sampling).
-# Here's the drill:
-# 1) encode input and retrieve initial decoder state
-# 2) run one step of decoder with this initial state
-# and a "start of sequence" token as target.
-# Output will be the next target token
-# 3) Repeat with the current target token and current states
-
-# Define sampling models
-encoder_model = Model(encoder_inputs, encoder_states)
-
-decoder_state_input_h = Input(shape=(latent_dim,))
-decoder_state_input_c = Input(shape=(latent_dim,))
-decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-decoder_outputs, state_h, state_c = decoder_lstm(
-    decoder_inputs, initial_state=decoder_states_inputs)
-decoder_states = [state_h, state_c]
-decoder_outputs = decoder_dense(decoder_outputs)
-decoder_model = Model(
-    [decoder_inputs] + decoder_states_inputs,
-    [decoder_outputs] + decoder_states)
-
-# Reverse-lookup token index to decode sequences back to
-# something readable.
-reverse_input_char_index = dict(
-    (i, char) for char, i in input_token_index.items())
-reverse_target_char_index = dict(
-    (i, char) for char, i in target_token_index.items())
+with io.open("train_set.txt", "r", encoding="utf-8") as f:
+    samples = f.readlines()
+    for sample in samples:
+        target_word, input_text, target_text = sample.split(" ")
+        target_text = '\t' + target_text + '\n'
+        input_texts_test.append(input_text)
+        target_texts_test.append(target_text)
+        target_words_test.append(target_word)
 
 
-def decode_sequence(input_seq, max_length):
-    print(input_seq.shape)
-    print(np.sum(input_seq))
-    # Encode the input as state vectors.
-    states_value = encoder_model.predict(input_seq)
-
-    # Generate empty target sequence of length 1.
-    target_seq = np.zeros((1, 1, num_decoder_tokens))
-    # Populate the first character of target sequence with the start character.
-    target_seq[0, 0, target_token_index['\t']] = 1.
-
-    # Sampling loop for a batch of sequences
-    # (to simplify, here we assume a batch of size 1).
-    stop_condition = False
-    decoded_sentence = ''
-    while not stop_condition:
-        output_tokens, h, c = decoder_model.predict(
-            [target_seq] + states_value)
-
-        # Sample a token
-        sampled_token_index = np.argmax(output_tokens[0, -1, :])
-        sampled_char = reverse_target_char_index[sampled_token_index]
-        decoded_sentence += sampled_char
-
-        # Exit condition: either hit max length
-        # or find stop character.
-        if (sampled_char == '\n' or
-           len(decoded_sentence) > max_length-1):
-            stop_condition = True
-
-        # Update the target sequence (of length 1).
-        target_seq = np.zeros((1, 1, num_decoder_tokens))
-        target_seq[0, 0, sampled_token_index] = 1.
-
-        # Update states
-        states_value = [h, c]
-
-    return decoded_sentence
-
-
-def decode_words(text, predicted):
-    decoded_words = ""
-    for i, p in enumerate(predicted):
-        if p == 'i':
-            decoded_words+=' '+text[i]
-        elif p == 's':
-            decoded_words+=text[i]+' '
-        else:
-            decoded_words+=text[i]
-    return decoded_words
-
-
-for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
-    input_seq = encoder_input_data[seq_index: seq_index + 1]
-    decoded_string = decode_sequence(input_seq, len(input_texts[seq_index]))
-    print('-')
-    print('Input words:' , ' '.join(target_words[seq_index].split("|")))
-    print('Input string:', input_texts[seq_index])
-    print('Decoded string:', decoded_string)
-    print('Decoded words:', decode_words(input_texts[seq_index], decoded_string))
+word_splitter = WordSplitter(input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test)
+word_splitter.prepare_data()
+word_splitter.train()
+word_splitter.save_model("encoder_model.json", "decoder_model.json", "encoder_weights.h5", "decoder_model_weights.h5")
+# word_splitter.load_model("encoder_model.json", "decoder_model.json", "encoder_weights.h5", "decoder_model_weights.h5", "reverse_target_char_index.p")
+# word_splitter.predict_on_test()
