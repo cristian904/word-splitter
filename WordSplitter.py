ecba80679eab7fcc517aaa7e873588e40ed11b92
Cristian Padurariu <cristian9004@Cristians-MacBook-Pro.local>
2019-12-18 15:57:24 +0200

    decoupled training and testing, added testing notebook

--- WordSplitter.py
+++ WordSplitter.py
@@ -5,25 +5,19 @@ import numpy as np
 import io
 import pickle
 
-batch_size = 100  # Batch size for training.
-epochs = 100  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 10000  # Number of samples to train on.
-
 class WordSplitter:
 
-    def __init__(self, input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test):
+    def __init__(self):
+        pass
+    
+    def prepare_train_data(self, input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test):
         self.input_texts_train = input_texts_train
         self.target_texts_train = target_texts_train
         self.target_words_train = target_words_train
         self.input_texts_test = input_texts_test
         self.target_texts_test = target_texts_test
         self.target_words_test = target_words_test
-        self.encoder_input_data_test = None
-        self.decoder_input_data_test = None
-        self.decoder_target_data_test = None
-    
-    def prepare_data(self):
+
         input_characters = set()
         target_characters = set()
         for input_text, target_text in list(zip(self.input_texts_train, self.target_texts_train)):
@@ -48,12 +42,12 @@ class WordSplitter:
 
         self.reverse_target_char_index = dict(
             (i, char) for char, i in self.target_token_index.items())
-        pickle.dump(self.reverse_target_char_index, open("reverse_target_char_index.p", "wb"))
+        
 
-        self.encoder_input_data_train, self.decoder_input_data_train, self.decoder_target_data_train = self.get_network_ready_data(self.input_texts_train, self.target_texts_train)
-        self.encoder_input_data_test, self.decoder_input_data_test, self.decoder_target_data_test = self.get_network_ready_data(self.input_texts_test, self.target_texts_test)
+        self.encoder_input_data_train, self.decoder_input_data_train, self.decoder_target_data_train = self.__get_network_ready_data(self.input_texts_train, self.target_texts_train)
+        self.encoder_input_data_test, self.decoder_input_data_test, self.decoder_target_data_test = self.__get_network_ready_data(input_texts_test, target_texts_test)
 
-    def get_network_ready_data(self, input_texts, target_texts):
+    def __get_network_ready_data(self, input_texts, target_texts):
         encoder_input_data = np.zeros(
             (len(input_texts), self.max_encoder_seq_length, self.num_encoder_tokens),
             dtype='float32')
@@ -79,11 +73,11 @@ class WordSplitter:
         
         return encoder_input_data, decoder_input_data, decoder_target_data
 
-    def __build_network(self):
+    def __build_network(self, latent_dim):
         # Define an input sequence and process it.
         encoder_inputs = Input(shape=(None, self.num_encoder_tokens))
         encoder = LSTM(latent_dim, return_state=True)
-        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        _, state_h, state_c = encoder(encoder_inputs)
         encoder_states = [state_h, state_c]
 
         decoder_inputs = Input(shape=(None, self.num_decoder_tokens))
@@ -109,7 +103,15 @@ class WordSplitter:
             [decoder_outputs] + decoder_states)
 
     
-    def save_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file):
+    def save_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file, constants_file):
+        constants = {"num_encoder_tokens": self.num_encoder_tokens,
+                    "num_decoder_tokens": self.num_decoder_tokens,
+                    "max_encoder_seq_length": self.max_encoder_seq_length,
+                    "max_decoder_seq_length": self.max_decoder_seq_length,
+                    "reverse_target_char_index": self.reverse_target_char_index,
+                    "input_token_index": self.input_token_index,
+                    "target_token_index": self.target_token_index}
+        pickle.dump(constants, open(constants_file, "wb"))
         model_json = self.encoder_model.to_json()
         with open(encoder_model_file, "w") as json_file:
             json_file.write(model_json)
@@ -119,9 +121,9 @@ class WordSplitter:
         self.encoder_model.save(encoder_weights_file)
         self.decoder_model.save(decoder_weights_file)
     
-    def train(self, ):
+    def train(self, batch_size, latent_dim, epochs):
 
-        self.__build_network()
+        self.__build_network(latent_dim)
         
         # Run training
         self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
@@ -134,9 +136,16 @@ class WordSplitter:
                 validation_data=([self.encoder_input_data_test, self.decoder_input_data_test], self.decoder_target_data_test))
 
 
-    def load_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file, reverse_target_char_index_file):
+    def load_model(self, encoder_model_file, decoder_model_file, encoder_weights_file, decoder_weights_file, constants_file):
 
-        self.reverse_target_char_index = pickle.load(open(reverse_target_char_index_file, "rb"))
+        constants = pickle.load(open(constants_file, "rb"))
+        self.num_encoder_tokens = constants["num_encoder_tokens"]
+        self.num_decoder_tokens = constants["num_decoder_tokens"]
+        self.max_encoder_seq_length = constants["max_encoder_seq_length"]
+        self.max_decoder_seq_length = constants["max_decoder_seq_length"]
+        self.reverse_target_char_index = constants["reverse_target_char_index"]
+        self.input_token_index = constants["input_token_index"]
+        self.target_token_index = constants["target_token_index"]
         loaded_encoder_json = open(encoder_model_file, "r").read()
         loaded_decoder_json = open(decoder_model_file, "r").read()
         self.encoder_model = model_from_json(loaded_encoder_json)
@@ -179,25 +188,42 @@ class WordSplitter:
     def decode_words(self, text, predicted):
         decoded_words = ""
         for i, p in enumerate(predicted):
-            if p == 'i':
-                decoded_words+=' '+text[i]
-            elif p == 's':
+            if p == 's':
                 decoded_words+=text[i]+' '
             else:
                 decoded_words+=text[i]
         return decoded_words
 
-    def predict_on_test(self):
-        for seq_index in range(len(self.input_texts_train)):
+    def predict_on_test(self, input_texts_test, target_texts_test, target_words_test, prediction_file, verbose):
+        import pandas as pd
+        input_words = []
+        input_strings = []
+        decoded_strings = []
+        decoded_wordss = []
+        encoder_input_data_test, _, _ = self.__get_network_ready_data(input_texts_test, target_texts_test)
+        for seq_index in range(len(input_texts_test)):
             # Take one sequence (part of the training set)
             # for trying out decoding.
-            input_seq = self.encoder_input_data_test[seq_index: seq_index + 1]
-            decoded_string = self.decode_sequence(input_seq, len(self.input_texts_test[seq_index]))
-            print('-')
-            print('Input words:' , ' '.join(self.target_words_test[seq_index].split("|")))
-            print('Input string:', self.input_texts_test[seq_index])
-            print('Decoded string:', decoded_string)
-            print('Decoded words:', self.decode_words(self.input_texts_test[seq_index], decoded_string))
+            input_seq = encoder_input_data_test[seq_index: seq_index + 1]
+            decoded_string = self.decode_sequence(input_seq, len(input_texts_test[seq_index]))
+            decoded_words = self.decode_words(input_texts_test[seq_index], decoded_string)
+            decoded_words = decoded_words.strip()
+            input_words.append(' '.join(target_words_test[seq_index].split("|")))
+            input_strings.append(input_texts_test[seq_index])
+            decoded_strings.append(decoded_string)
+            decoded_wordss.append(decoded_words)
+            if verbose:
+                print('-')
+                print('Input words:' , ' '.join(target_words_test[seq_index].split("|")))
+                print('Input string:', input_texts_test[seq_index])
+                print('Decoded string:', decoded_string)
+                print('Decoded words:', self.decode_words(input_texts_test[seq_index], decoded_string))
+        df = pd.DataFrame()
+        df['Input words'] = input_words
+        df['Input string'] = input_strings
+        df['Decoded string'] = decoded_strings
+        df['Decoded words'] = decoded_wordss
+        df.to_excel(prediction_file, index=False)
 
 
         
@@ -229,9 +255,9 @@ with io.open("./data/test_set.txt", "r", encoding="utf-8") as f:
         target_words_test.append(target_word)
 
 
-word_splitter = WordSplitter(input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test)
-word_splitter.prepare_data()
-word_splitter.train()
-word_splitter.save_model("./models/encoder_model.json", "./models/decoder_model.json", "./models/encoder_weights.h5", "./models/decoder_model_weights.h5")
-# word_splitter.load_model("encoder_model.json", "decoder_model.json", "encoder_weights.h5", "decoder_model_weights.h5", "reverse_target_char_index.p")
-# word_splitter.predict_on_test()
+word_splitter = WordSplitter()
+# word_splitter.prepare_train_data(input_texts_train, target_texts_train, target_words_train, input_texts_test, target_texts_test, target_words_test)
+# word_splitter.train(batch_size=1000, latent_dim=256, epochs=1)
+# word_splitter.save_model("./models/encoder_model.json", "./models/decoder_model.json", "./models/encoder_weights.h5", "./models/decoder_model_weights.h5", "./models/constants.p")
+word_splitter.load_model("./models/encoder_model.json", "./models/decoder_model.json", "./models/encoder_weights.h5", "./models/decoder_model_weights.h5", "./models/constants.p")
+word_splitter.predict_on_test(input_texts_test, target_texts_test, target_words_test, "./output/test_set_predictions.xlsx", False)
